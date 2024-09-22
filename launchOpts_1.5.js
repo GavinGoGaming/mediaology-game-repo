39af77f601a474263fe85a6df9b6ecc48567fe31
Gavin Fox <gavingogaming@gmail.com>
2024-09-22 09:25:49 -0700

    Update launchOpts_1.5.js

--- launchOpts_1.5.js
+++ launchOpts_1.5.js
@@ -49,4 +49,21 @@ setTimeout(()=>{
 document.querySelector("#_gl_c2s").addEventListener('click', (e)=>{
     document.querySelector("#_gl_div").remove();
 });
+const e = document.createElement('button');
+    e.style.width = "12px";
+    e.style.height = "12px";
+    e.style.position = "absolute";
+    e.style.top = "0px";
+    e.style.left = "0px";
+    e.style.zIndex = "9999";
+    e.style.border = "1px solid white";
+    e.style.background = "#333";
+    e.style.borderRadius = "6px";
+    e.addEventListener('click', ()=>{
+    document.body.insertAdjacentHTML("afterbegin", `<div id="_gl_div" style="width:100vw;height:100vh;z-index:9999;background:#111;color:white;display:flex;justify-content:center;align-items:center;position:absolute;top:0;left:0;"><p id="_gl_c2s">click me to start</p></div>`);
+document.querySelector("#_gl_c2s").addEventListener('click', (e)=>{
+    document.querySelector("#_gl_div").remove();
+});
+    });
+document.body.appendChild(e);
 },500);
